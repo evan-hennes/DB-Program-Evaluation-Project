8cc0d25799438b17888ad60a4f6772c80cdfb834
Pravin Ramana <pravinramana25@protonmail.ch>
2023-12-02 20:23:31 -0600

    Split up the initializer steps and create separate frames for querying and adding data

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 import tkinter as tk
 
-def submit(cmdVar):
+
+# Place frame setup code here
+
+def setup_data_entry_frame(parent, status_label):
+    entry_frame = tk.Frame(parent, borderwidth=2, relief='ridge')
+    entry_frame.pack(side='left', fill='both', expand=True)
+
+    title_entry = tk.Label(entry_frame, text='Data Entry', font='Helvetica 18 bold')
+    title_entry.pack()
+
+    cmdLabel = tk.Label(entry_frame, text='Enter your command:', font='Helvetica 15')
+    cmdLabel.pack()
+
+    cmd = tk.Entry(entry_frame)
+    cmd.pack()
+
+    submit_button = tk.Button(entry_frame, text="Submit", command=lambda: submit(cmd, status_label))
+    submit_button.pack()
+
+    return entry_frame
+
+def setup_data_query_frame(parent):
+    query_frame = tk.Frame(parent, borderwidth=2, relief='ridge')
+    query_frame.pack(side='right', fill='both', expand=True)
+
+    title_query = tk.Label(query_frame, text='Data Query', font='Helvetica 18 bold')
+    title_query.pack()
+
+    return query_frame
+
+# Database functions and user input section
+
+def add_data(cmdText, status_label):
+    status_label.config(text=f'Added Data: {cmdText}')
+    print(f'Adding Data: {cmdText}')
+
+def query_data(cmdText, status_label):
+    status_label.config(text=f'Queried Data: {cmdText}')
+    print(f'Querying Data: {cmdText}')
+
+def submit(cmdVar, status_label):
     cmdText = cmdVar.get()
-    print(f'Command Entered: {cmdText}')
-    # commands to implement:
-    # d = enter department info
-    # f = enter faculty info
-    # p = enter program info
-    # c = enter course info
-    # s = enter section info
-    # l = enter learning objective/sub-objective info
-    # a = add course to program
-    # A = ade (sub)objectives to (course, program) pairs
-    # e = enter evaluation results for section
-    # D = get department info
-    # P = get program info
-    # S = get evaluation results for each section given semester & program
-    # Y = get evaluation results for each objective/sub-objective given academic year
+    status_label.config(text=f'TODO! Your input was {cmdText}')
+
 
 if __name__ == '__main__':
     window = tk.Tk()
-    title = tk.Label(text='Program Evaluation', font='Helvetica 18 bold')
-    title.pack()
-    cmdLabel = tk.Label(text='Enter your command:', font='Helvetica 15')
-    cmdLabel.pack()
-    cmd = tk.Entry()
-    cmd.pack()
-    submit_button = tk.Button(window, text="Submit", command=lambda: submit(cmd))
-    submit_button.pack()
+    window.title('Program Evaluation')
+
+    # Status Label
+    status_label = tk.Label(window, text='', font='Helvetica 12', fg='red')
+    status_label.pack()
+
+    # Set up frames
+    entry_frame = setup_data_entry_frame(window, status_label)
+    query_frame = setup_data_query_frame(window)
+
     window.mainloop()
+
