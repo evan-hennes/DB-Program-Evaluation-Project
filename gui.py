3933e9e49968ce7bcc91c18be80d528909b48b83
Pravin Ramana <pravinramana25@protonmail.ch>
2023-12-03 20:03:40 -0600

    Update gui with more fields

--- gui.py
+++ gui.py
@@ -16,15 +16,27 @@ def validate_email(entry):
     return "@" in entry.get().strip() and "." in entry.get().strip()
 
 
-validation_functions = {"Code": validate_department_code, "Email": validate_email}
+def validate_course_id(entry):
+    # Example validation for course ID (can be improved)
+    return entry.get().strip().isalnum() and len(entry.get().strip()) == 7
+
+
+def validate_section_id(entry):
+    # Example validation for section ID (can be improved)
+    return entry.get().strip().isdigit() and len(entry.get().strip()) <= 3
+
+
+validation_functions = {
+    "Code": validate_department_code,
+    "Email": validate_email
+}
 
 
 def check_entries(entries, submit_button):
     all_valid = all(
         validate_non_empty(entry)
         and validation_functions.get(field, lambda e: True)(entry)
-        for field, entry in entries.items()
-    )
+        for field, entry in entries.items())
     submit_button.config(state="normal" if all_valid else "disabled")
 
 
@@ -52,8 +64,8 @@ def add_data_fields(tab, field_names, validation_functions, status_label):
         entry = tk.Entry(frame)
         entry.pack(side="right", expand=True, fill="x")
         entry.bind(
-            "<KeyRelease>", lambda event, e=entry: check_entries(entries, submit_button)
-        )
+            "<KeyRelease>",
+            lambda event, e=entry: check_entries(entries, submit_button))
         entries[field] = entry
 
     submit_button = tk.Button(
@@ -83,9 +95,10 @@ def add_faculty_fields(tab, field_names, validation_functions, status_label):
             # Dropdown for Rank selection
             rank_var = tk.StringVar()
             rank_options = ["full", "associate", "assistant", "adjunct"]
-            rank_dropdown = ttk.Combobox(
-                frame, textvariable=rank_var, values=rank_options, state="readonly"
-            )
+            rank_dropdown = ttk.Combobox(frame,
+                                         textvariable=rank_var,
+                                         values=rank_options,
+                                         state="readonly")
             rank_dropdown.set(rank_options[0])  # Set default value
             rank_dropdown.pack(side="right", expand=True, fill="x")
             entries[field] = rank_dropdown
@@ -133,9 +146,36 @@ def setup_data_entry_tab(notebook, status_label):
     faculty_tab = ttk.Frame(data_entry_notebook)
     data_entry_notebook.add(faculty_tab, text="Faculty")
     faculty_fields = ["ID", "Name", "Email", "Rank"]
-    add_faculty_fields(
-        faculty_tab, faculty_fields, {"Email": validate_email}, status_label
-    )
+    add_faculty_fields(faculty_tab, faculty_fields, {"Email": validate_email},
+                       status_label)
+
+    programs_tab = ttk.Frame(data_entry_notebook)
+    data_entry_notebook.add(programs_tab, text="Programs")
+    program_fields = ["Name", "Department ID", "Person in Charge ID"]
+    add_data_fields(programs_tab, program_fields, {}, status_label)
+
+    courses_tab = ttk.Frame(data_entry_notebook)
+    data_entry_notebook.add(courses_tab, text="Courses")
+    course_fields = ["ID", "Title", "Description", "Department ID"]
+    add_data_fields(courses_tab, course_fields, {"ID": validate_course_id},
+                    status_label)
+
+    sections_tab = ttk.Frame(data_entry_notebook)
+    data_entry_notebook.add(sections_tab, text="Sections")
+    section_fields = [
+        "ID",
+        "Course ID",
+        "Semester",
+        "Instructor ID",
+        "Enrollment Count",
+    ]
+    add_data_fields(sections_tab, section_fields, {"ID": validate_section_id},
+                    status_label)
+
+    objectives_tab = ttk.Frame(data_entry_notebook)
+    data_entry_notebook.add(objectives_tab, text="Learning Objectives")
+    objective_fields = ["Code", "Description", "Parent Objective Code"]
+    add_data_fields(objectives_tab, objective_fields, {}, status_label)
 
     # Add the rest later
 
